7370c11d5c4407744bd996f451c11e3919b4b125
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-02-27 04:10:57 -0300

    routes

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,9 @@ server.get("/", (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
 })
 
+server.get("/criancas/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  res.json({ message: 'Selecionou o pijama: ' + id });
+})
+
 server.listen(3000);
