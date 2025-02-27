6b2f2b606e00dc1b6ff81f60721bc7b54b63e200
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-02-27 04:01:14 -0300

    first route - GET

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("teste")
+import express, { Request, Response } from "express"
+
+const server = express();
+
+server.get("/", (req: Request, res: Response) => {
+  res.json({ message: 'Hello World!' });
+})
+
+server.listen(3000);
