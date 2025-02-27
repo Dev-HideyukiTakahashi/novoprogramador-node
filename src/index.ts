6d0b89547ade144092deb324190972f185271a2e
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-02-27 05:18:38 -0300

    404 not found

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ const server = express();
 
 server.use(express.json())
 server.use(router)
+server.use((req, res) => {
+  res.status(404).json({ message: 'Rota não encontrada' });
+})
 
 server.listen(3000);
