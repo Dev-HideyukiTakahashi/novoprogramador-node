ef834ce9d86a50f58b1ff159bad568440f8db742
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-02 20:51:12 -0300

    jwt example

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
 import express from 'express';
 import router from './routes';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
+
+// JWT
+const payload = {
+  userId: '95bdb07c-91c4-4063-934c-ac9fe41aefbb',
+  name: 'John Doe'
+};
+
+const secret = 'f2abb721-338a-4409-a4a1-3358c6b89970';
+const token = jwt.sign(payload, secret, { expiresIn: '1h' });
+console.log(token);
+
+try {
+  const decoded = jwt.verify(token, secret);
+  console.log("Decoded JWT: ", decoded);
+} catch (err) {
+  console.log(err);
+}
+
+
+// BCRYPT
 const pass = 'minhasenha123';
 const saltRounds = 10;
 
